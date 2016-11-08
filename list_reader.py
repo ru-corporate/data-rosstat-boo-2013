b2e467c8699a186d3a258b4bb6ab99dc75094028
=epogrebnyak <e.pogrebnyak@gmail.com>
2016-11-08 09:30:03 +0300

    remote and list_reader

---
+++ list_reader.py
@@ -0,0 +1,109 @@
+"""
+   Read source CSV file and adjust numeric units.
+   
+"""
+
+from remote import RemoteDataset
+from column_names import COLNAMES  
+
+# check if file is downloaded and get filenames
+year = 2013
+rd = RemoteDataset(year).download().unrar()
+raw_data_filename, clean_data_csv_path = rd.get_csv_filenames()
+
+string_columns  = COLNAMES[0:8] # ['name', 'okpo', 'okopf', 'okfs', 'okved', 'inn', 'unit', 'report_type' ]
+numeric_columns = COLNAMES[8:]  # ['11103', '11104', '11203'...
+
+
+def get_csv_lines(filename=SOURCE_CSV_PATH, cols=COLNAMES):
+    """Read CSV file"""
+    with open(filename) as f:
+        for line in f:
+            if ";" in line:
+               text_values = line.strip().split(";")
+               yield dict(zip(cols,text_values)) 
+
+def get_csv_lines(filename):
+    with open(filemame, 'rb') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=';')
+        for row in spamreader:
+           yield row
+           
+           
+           
+
+
+
+# write headers to csv file 
+pd.DataFrame(columns=COLNAMES).to_csv(TARGET_CSV_PATH, encoding='utf-8')
+
+# parameters of source csv file import
+cre = dict(filepath_or_buffer=SOURCE_CSV_PATH, 
+           sep=';', 
+           encoding="cp1251",
+           names=COLNAMES, 
+           dtype={'inn':str})
+
+chunks = pd.read_csv(**cre,chunksize=100000)
+for i, df in enumerate(chunks):
+
+    # uncomment when debugging
+    #if i > 0: break
+   
+    # delete last line with nulls
+    # Note: I was trying to get rid of this line with 'skipfooter=1',
+    #       but then I was getting a conflict with 'dtype' specification
+    if any(df.iloc[-1:].isnull()):
+       df=df[:-1]
+    
+    # adjust numeric units
+    # we need to multiply numeric values in several rows by a factor of .001 or 1000
+    multipliers={383:0.001,  385:1000}
+    for k, m in multipliers.items(): 
+        index = (df.unit==int(k)) | (df.unit==str(k)) 
+        df.loc[index,numeric_columns] = df.loc[index,numeric_columns].multiply(m).round(0)
+    
+    df.to_csv(TARGET_CSV_PATH, mode='a', encoding='utf-8', header = False)
+
+
+# Problem 1: adjusting numeric units in lines 42-46 
+#     (a) seems very slow, 
+#     (b) I'm using iloc, but still getting a SettingWithCopyWarning: 
+#         A value is trying to be set on a copy of a slice from a DataFrame.
+#         Try using .loc[row_indexer,col_indexer] = value instead  
+
+# Problem 2: 
+#     for numeric values the data type is integer, not float
+#     in my source file I have to last line of NA values, so I cannot use dtype=int, nor skipfooter=1 
+#     in a csv file I want to keep all numeric columns ('numeric_columns') as int type
+#     the resulting file TARGET_CSV_PATH is about 2 time bigger than original SOURCE_CSV_PATH file,
+#     which is very upsetting 
+
+
+# Below is not todo.
+
+
+#ff = pd.read_csv(OUTPUT)
+
+## 
+##   String transformation functions 
+##           
+
+#def get_okved_dict(code_string):
+#    """Extract 3 levels of okved code from code_string line 80.10.02"""
+#    def _okved_tuple(code_string): 
+#        codes = [int(x) for x in str(code_string).split(".")]
+#        return codes + [None] * (3-len(codes))        
+#    return dict(zip(OKVED_KEYS, _okved_tuple(code_string)))
+#
+#def dequote(line):
+#    """Split company name to organisation and tiile"""
+#    parts = line.split(QUOTE_CHAR)
+#    org_type = parts[0].strip()
+#    new_line = QUOTE_CHAR.join(parts[1:-1])
+#    if new_line.count(QUOTE_CHAR)==1:
+#        new_line = new_line + QUOTE_CHAR
+#    if not new_line:
+#       new_line = line         
+#    return org_type, new_line.strip()    
+#    
---
+++ remote.py
@@ -0,0 +1,82 @@
+"""Download + unpack CSV file."""
+
+import requests
+import os
+import subprocess
+
+from config import UNPACK_RAR_EXE
+
+from config import DATA_DIR
+RAR_DIR = DATA_DIR
+CSV_DIR = DATA_DIR
+
+LOCATIONS = {
+   2013 : dict(url="http://www.gks.ru/opendata/storage/7708234640-bdboo2013/data-20150707t000000-structure-20131231t000000.rar"
+        , csv_file="G2013.csv")
+}
+
+
+class RemoteDataset():   
+        
+    def get_rar_local_path(self):
+        filename = self.url.split('/')[-1]
+        return os.path.join(RAR_DIR, filename)
+
+    def get_modified_csv_local_path(self):
+        self.csv_path
+    
+    def __init__(self, year):
+        self.url = LOCATIONS[year]['url']
+        # RAR filename 
+        rar_filename = self.url.split('/')[-1]          
+        self.rar_path = os.path.join(RAR_DIR, rar_filename)
+        # Original CSV filename        
+        csv_filename = LOCATIONS[year]['csv_file']
+        self.source_csv_path = os.path.join(CSV_DIR, csv_filename)
+        # New CSV filename
+        base, ext = csv_filename.split(".")
+        csv_filename_2 = base + "_ready." + ext
+        self.modified_csv_path = os.path.join(CSV_DIR, csv_filename_2)
+
+    @staticmethod    
+    def _download(url, path):
+        r = requests.get(url.strip(), stream=True)
+        with open(path, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=1024): 
+                if chunk: # filter out keep-alive new chunks
+                    f.write(chunk)
+        return path
+
+    @staticmethod
+    def _unrar(path, folder):        
+        subprocess.check_call([
+            UNPACK_RAR_EXE,
+            'e', path,
+            folder,
+            '-y'
+        ]) 
+
+    def download(self, overwrite=False):
+        if os.path.exists(self.rar_path) or not overwrite:
+            print("Already downloaded:", self.rar_path)
+        else:
+            print("Downloading:", self.url)
+            self._download(self.url, self.rar_path)    
+            print("Saved as:", self.rar_path)        
+        return self    
+
+    def unrar(self, overwrite = False):
+        if os.path.exists(self.source_csv_path) or not overwrite:
+            print("Already unpacked:", self.source_csv_path)
+        else:         
+            self._unrar(self.rar_path, RAR_DIR) 
+        return self
+        
+    def get_csv_filenames(self):
+        return self.source_csv_path, self.modified_csv_path
+            
+if __name__ == "__main__":
+    rd = RemoteDataset(2013).download().unrar()
+    raw_data_csv_path, clean_data_csv_path = rd.get_csv_filenames()   
+    assert raw_data_csv_path == 'data\\G2013.csv'
+    assert clean_data_csv_path == 'data\\G2013_ready.csv'
