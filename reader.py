a97a658697e10941ee35f211df3327178b47d0fc
carolinux <carolinegr@gmail.com>
2016-11-11 18:17:28 +0100

    Adds comments

--- reader.py
+++ reader.py
@@ -61,6 +61,13 @@ class Downloader():
         return self
     
     @staticmethod
+    # COMMENT: Having a subprocess works quite good
+    # I looked at python solutions, they all seem to need
+    # unrar.exe to be in the path anyway
+    # For linux, if unrar is not installed subprocess will fail.
+    # I would suggest to use .zip files instead and then install
+    # the zipfile package (which is a python package)
+    # so no external calls are needed
     def _unrar(path, folder=SOURCE_CSV_DIR):        
         subprocess.check_call([
             UNPACK_RAR_EXE,
@@ -115,7 +122,10 @@ def get_csv_lines(filename=SOURCE_CSV_PATH, cols=COLNAMES):
 #   Manipulate labels
 #           
 #            
-               
+
+# COMMENT: This logic here is used to initialize the OUTPUT_CSV_COLUMNS
+# but is a bit confusing - perhaps it can be put in a function, like:
+# OUPUT_CSV_COLUMNS = generate_output_csv_columns()
 data_fields = [x[0:-1] for x in current]
 data_labels = current+prev
 new = ['year'] + OKVED_KEYS + ['region', 'org', 'title']   
@@ -130,6 +140,10 @@ OUTPUT_CSV_COLUMNS = new + firm + data_fields
 #           
 # 
 
+# COMMENT: I suppose here you are not using pandas because of memory concerns?
+# Because getting the rows as dictionaries is very easy in a pandas DataFrame
+
+
 def lines_as_dicts(filename=SOURCE_CSV_PATH, cols=COLNAMES, year=YEAR, 
                    yield_previous_year=False):
                        
@@ -137,6 +151,9 @@ def lines_as_dicts(filename=SOURCE_CSV_PATH, cols=COLNAMES, year=YEAR,
 
     unit_multipliers={'383':0.001, '384':1, '385':1000}
     
+    # COMMENT: Here, prev and current looks like it's a variable, whereas it's really a constant 
+    # imported from a module. You can rename to PREV_YEAR_INDEX and CURR_YEAR_INDEX (or something else
+    # that is an informative name)
     if yield_previous_year:
         year=year-1
         ix = prev
@@ -170,6 +187,13 @@ def lines_as_dicts(filename=SOURCE_CSV_PATH, cols=COLNAMES, year=YEAR,
         yield r
 
 
+# COMMENT:
+# I would rename this to: output_to_csv
+# COMMENT:
+# Reading/Writing the data one row at a time is good for memory usage
+# But there's a hybrid solution where you read N lines and write N lines together
+# which can have better performance (because writing takes the same time if you write a small chunk of a data
+# vs a slightly bigger one. So, if writing takes a lot of time, writing multiple rows at once is something to consider
 def to_csv(gen, filename, folder=SOURCE_CSV_DIR, cols=OUTPUT_CSV_COLUMNS):
     path = os.path.join(folder, filename)
     with open(path, 'w', encoding = "utf-8") as output_file:    
@@ -182,6 +206,8 @@ def to_csv(gen, filename, folder=SOURCE_CSV_DIR, cols=OUTPUT_CSV_COLUMNS):
 
 
 if __name__=="__main__":
+    # COMMENT: This is the entrypoint to the whole thing. I would actually put explicitly in the keyword arguments
+    # because without them it's not clear what you are getting and where you are saving it
     Downloader(SOURCE_URL).download().unrar()
     to_csv(gen=lines_as_dicts(), filename="all2013.csv")   
     to_csv(gen=lines_as_dicts(yield_previous_year=True), filename="all2012.csv")
