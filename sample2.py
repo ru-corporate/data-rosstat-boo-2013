3de96f492b4f78c9cd92562a97a046e7a1d8fbf2
=epogrebnyak <e.pogrebnyak@gmail.com>
2016-11-27 22:14:58 +0300

    added sample 2

--- sample2.py
+++ sample2.py
@@ -7,7 +7,7 @@ Created on Sun Nov 27 15:36:24 2016
 
 from reader import Dataset
 #df = Dataset(2015).read_df()
-df0 = Dataset(2013).read_df()
+#df0 = Dataset(2013).read_df()
 df.nlargest(20, 'ta')[['title','inn', 'unit']]
 
 # suspicious
@@ -15,6 +15,9 @@ df[(df.tp_cap == df.tp) & (df.ta==df.ta_fix)][['title','ta']].sort_values('ta')
 
 
 #other suspicious inns
-bad_inns = ['7702844336', '7710244903', '7707089648','7707322083']
+bad_inns = ['7702844336', '7710244903', '7707089648', '7707322083']
 
 df[df.inn.isin(bad_inns)].transpose()
+
+#7707089648
+#http://kommersant.ru/doc/125895
